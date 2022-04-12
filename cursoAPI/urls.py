8d521fd6dfc84d00bbd0ebcaf10270d6e019f0e2
tiago <tiagohbello@gmail.com>
2022-04-12 08:13:38 -0300

    finalização da API

--- cursoAPI/urls.py
+++ cursoAPI/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from escola.views import AlunosViewSet,CursosViewSet, MatriculaViewSet, ListaMatriculasAluno, ListaAlunosMatriculados
+from rest_framework import routers
 
+router = routers.DefaultRouter()
+router.register('alunos', AlunosViewSet, basename='alunos')
+router.register('cursos',CursosViewSet, basename='cursos')
+router.register('matriculas', MatriculaViewSet, basename='matriculas')
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('alunos/<int:pk>/matriculas/', ListaMatriculasAluno.as_view()),
+    path('cursos/<int:pk>/matriculas/', ListaAlunosMatriculados.as_view())
 ]
